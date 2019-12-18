ebe2e2153697799a344a93e20e7151d640720046
dellahi elwaghf <dellahiwakhfe@gmail.com>
2019-12-18 03:40:20 +0100

    first commit

---
+++ tp5/src/main/java/fr/ensim/tp5/controller/AddressController.java
@@ -0,0 +1,19 @@
+package fr.ensim.tp5.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import fr.ensim.tp5.model.AddressRepository;
+
+@Controller
+public class AddressController {
+@Autowired
+AddressRepository addressRepository ;
+@GetMapping ("/adresses")
+public String showAddresses ( Model model ) {
+model.addAttribute("allAdresses", addressRepository.findAll ());
+return "adresses";
+}
+}
---
+++ tp5/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO address (id , creation , content, auteur ) VALUES (1,
+CURRENT_TIMESTAMP () , '57 boulevard demorieux ', 'Auth1');
+INSERT INTO address (id , creation , content, auteur ) VALUES (2,
+CURRENT_TIMESTAMP () , '51 allee du gamay , 34080
+montpellier ', 'Auth2');
